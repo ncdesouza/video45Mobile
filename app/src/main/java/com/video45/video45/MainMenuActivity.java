069ee419590a4870f62633f884e44c3a3b382993
RosettM <matthew.rosettis@gmail.com>
2015-10-29 13:37:19 -0400

    Formatted main menu to play a video in the background, Added a landscape orientation for main menu with button reposition

--- app/src/main/java/com/video45/video45/MainMenuActivity.java
+++ app/src/main/java/com/video45/video45/MainMenuActivity.java
@@ -2,8 +2,12 @@ package com.video45.video45;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
+import android.net.Uri;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class MainMenuActivity extends Activity {
 
@@ -11,14 +15,13 @@ public class MainMenuActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
-//        InputStream stream = null;
-//        try{
-//            stream.getAssets().open("main_menu.gif");
-//        }catch (IOException e){
-//            e.printStackTrace();
-//        }
-//        GifMovieView view = new GifMovieView(this, stream);
-//        setContentView(view);
+        //backgroundVideo();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        backgroundVideo();
     }
 
     public void loginPress(View view){
@@ -29,5 +32,24 @@ public class MainMenuActivity extends Activity {
     public void signUpPress(View view){
         Intent signUpIntent = new Intent(this, SignUpActivity.class);
         startActivity(signUpIntent);
+    }
+    VideoView vidView;
+    public void backgroundVideo(){
+        String vidAddress;
+        vidView = (VideoView)findViewById(R.id.menuVideo);
+        //Video Loop
+        vidView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            public void onCompletion(MediaPlayer mp) {
+                vidView.start(); //need to make transition seamless.
+            }
+        });
+        //TODO Make this into an array and add more videos to be looped
+        vidAddress =
+//            "https://archive.org/download/ksnn_compilation_master_the_internet/ksnn_compilation_master_the_internet_512kb.mp4";
+            "http://video45.cloudapp.net/public/vid/dock.mp4";
+
+        Uri vidUri = Uri.parse(vidAddress);
+        vidView.setVideoURI(vidUri);
+        vidView.start();
     }
 }
