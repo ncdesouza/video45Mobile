b7fa5bef44103b5f5bc12bb84e3952e988e8646c
ncdesouza <nicholas.desouza@uoit.net>
2015-10-30 14:37:14 -0400

    edited Manifest

---
+++ app/src/main/java/com/video45/video45/tools/image/ImageTask.java
@@ -0,0 +1,27 @@
+package com.video45.video45.tools.image;
+
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
+
+import java.net.URL;
+import java.net.URLConnection;
+
+/**
+ * Created by nicholas on 30/10/15.
+ */
+public class ImageTask extends AsyncTask<String, String, Bitmap> {
+    @Override
+    protected Bitmap doInBackground(String... params) {
+//        try {
+//
+//        }
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Bitmap image) {
+
+    }
+
+
+}
