a1c73cc2faeb25c03795c290a86d440395c15a52
ncdesouza <nicholas.desouza@uoit.net>
2015-10-30 19:39:01 -0400

    added side nav

--- app/src/main/java/com/video45/video45/profile/ProfileActivity.java
+++ app/src/main/java/com/video45/video45/profile/ProfileActivity.java
@@ -1,20 +1,54 @@
 package com.video45.video45.profile;
 
 import android.content.Intent;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.video45.video45.R;
+import com.video45.video45.tools.nav.DrawerItemClickListener;
 import com.video45.video45.tools.video.VideoAdapter;
 
 public class ProfileActivity extends AppCompatActivity implements ProfileListener{
+    private String[] navOptions;
+    private ListView navList;
+    private DrawerLayout navDrawer;
+    private ActionBarDrawerToggle navToggle;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         getProfile();
+
+        navOptions = getResources().getStringArray(R.array.nav_options);
+        navDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        navList = (ListView) findViewById(R.id.left_drawer);
+
+        navList.setAdapter(new ArrayAdapter<String>(this, R.layout.nav_list_item, navOptions));
+        navList.setOnItemClickListener(new DrawerItemClickListener());
+
+        navToggle = new ActionBarDrawerToggle(this, navDrawer, R.string.nav_open, R.string.nav_close) {
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+
+        navDrawer.setDrawerListener(navToggle);
+
+
+
     }
 
     private void getProfile() {
---
+++ app/src/main/java/com/video45/video45/tools/nav/DrawerItemClickListener.java
@@ -0,0 +1,19 @@
+package com.video45.video45.tools.nav;
+
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+/**
+ * Created by nicholas on 30/10/15.
+ */
+public class DrawerItemClickListener implements ListView.OnItemClickListener {
+    @Override
+    public void onItemClick(AdapterView parent, View view, int position, long id) {
+        selectItem(position);
+    }
+
+    private void selectItem(int position) {
+
+    }
+}
